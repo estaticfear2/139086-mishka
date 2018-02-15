7e36b0c0b594764a19f989e9094bdc74e320cb89
Slava Slivka <aphelion.unet@gmail.net>
2018-02-15 23:34:02 +0200

    m10-t1

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,47 +1,62 @@
-var ESC_CODE = 27;
+(function () {
+  var ESC_CODE = 27;
 
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
-var buyLink = document.querySelector('.js-add-to-cart');
-var modal = document.querySelector('.modal-content');
+  var navMain = document.querySelector('.main-nav');
+  var navToggle = document.querySelector('.main-nav__toggle');
+  var buyLink = document.querySelector('.js-add-to-cart');
+  var modal = document.querySelector('.modal-content');
 
-navMain.classList.remove('main-nav--no-js');
+  navMain.classList.remove('main-nav--no-js');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
-
-var onEscPress = function (evt) {
-  if (evt.keyCode == ESC_CODE) {
-    modal.classList.remove('modal-content--open');
-    document.removeEventListener('keydown', onEscPress);
-  }
-};
-
-var onOverlayClick = function (evt) {
-  var clickedElement = evt.target.closest('.modal-content__wrap');
-  if (!clickedElement) {
-    modal.classList.remove('modal-content--open');
-    modal.removeEventListener('click', onOverlayClick);
-  }
-};
-
-var currentPage = document.querySelector('body').classList;
-
-if (currentPage.contains('page-index')) {
-  buyLink.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    modal.classList.add('modal-content--open');
-    document.addEventListener('keydown', onEscPress);
-    modal.addEventListener('click', onOverlayClick);
+  navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
   });
-}
 
+  var onEscPress = function (evt) {
+    if (evt.keyCode == ESC_CODE) {
+      modal.classList.remove('modal-content--open');
+      document.removeEventListener('keydown', onEscPress);
+    }
+  };
 
+  var onOverlayClick = function (evt) {
+    var clickedElement = evt.target.closest('.modal-content__wrap');
+    if (!clickedElement) {
+      modal.classList.remove('modal-content--open');
+      modal.removeEventListener('click', onOverlayClick);
+    }
+  };
 
+  var currentPage = document.querySelector('body').classList;
+  var productsList = document.querySelector('.products__list');
+
+  if (currentPage.contains('page-index')) {
+    buyLink.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      modal.classList.add('modal-content--open');
+      document.addEventListener('keydown', onEscPress);
+      modal.addEventListener('click', onOverlayClick);
+    });
+  }
+
+  if (currentPage.contains('catalog-page')) {
+    productsList.addEventListener('click', function (evt) {
+
+      var clickedBasket = evt.target.closest('.product__buy');
+
+      if (clickedBasket) {
+        evt.preventDefault();
+        modal.classList.add('modal-content--open');
+        document.addEventListener('keydown', onEscPress);
+        modal.addEventListener('click', onOverlayClick);
+        return;
+      }
+    });
+  }
+})();
